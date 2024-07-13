081fc21b15e3bb6281048150b7c03f739c4abde7
marciojr <marciomendescjr@gmail.com>
2024-07-13 00:02:05 -0300

    feat: add golang message

--- init/init.go
+++ init/init.go
@@ -12,7 +12,7 @@ func main() {
 	log.SetPrefix("init: ")
 	log.SetFlags(0)
 
-	msg, err := message.ShowMessage("")
+	msg, err := message.ShowMessage("Golang is a great lang")
 
 	if err != nil {
 		log.Fatal(err)
