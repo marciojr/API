7dd297e850b02334c3b8d94e760bfe6452c553a1
marciojr <marciomendescjr@gmail.com>
2024-07-21 00:21:03 -0300

    refactor: fix name of expenses attr

--- data/user.go
+++ data/user.go
@@ -4,15 +4,15 @@ type User struct {
 	ID       int        `json:"id"`
 	Name     string     `json:"name"`
 	Balance  float64    `json:"balance"`
-	Expences []Expenses `json:"expenses"`
+	Expenses []Expenses `json:"expenses"`
 }
 
 func GetUsers() []User {
 
 	var users = []User{
-		{ID: 1, Name: "John", Balance: 1000.00, Expences: GetFirstExpense()},
-		{ID: 2, Name: "Megan", Balance: 5000.00, Expences: GetSecondExpense()},
-		{ID: 3, Name: "Michael", Balance: 15000.00, Expences: GetThirdExpense()},
+		{ID: 1, Name: "John", Balance: 1000.00, Expenses: GetFirstExpense()},
+		{ID: 2, Name: "Megan", Balance: 5000.00, Expenses: GetSecondExpense()},
+		{ID: 3, Name: "Michael", Balance: 15000.00, Expenses: GetThirdExpense()},
 	}
 
 	return users
