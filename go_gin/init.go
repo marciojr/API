06ea5655a8706ca62f6c5975c37b5d18c284654e
marciojr <marciomendescjr@gmail.com>
2024-07-21 00:54:13 -0300

    feat: add POST /users endpoint Enable to add user by POST /users endpoint

--- go_gin/init.go
+++ go_gin/init.go
@@ -10,9 +10,22 @@ import (
 func Initialize() {
 	r := gin.Default()
 	r.GET("/users", getUsers)
+	r.POST("/users", postUser)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func getUsers(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, data.GetUsers())
+	c.IndentedJSON(http.StatusOK, data.Users)
+}
+
+func postUser(c *gin.Context) {
+	var newUser data.User
+
+	if err := c.BindJSON(&newUser); err != nil {
+		return
+	}
+
+	data.AddUser(newUser)
+
+	c.IndentedJSON(http.StatusCreated, newUser)
 }
